72b55a266a75206af7ef10d34fff7044bfb96be6
Ramya Mallina <malli@MacBook-Air.local>
2024-04-25 13:45:11 -0400

    Add ratings module functionality and update main.py

--- finalProject/api/main.py
+++ finalProject/api/main.py
@@ -2,9 +2,10 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routers import index as indexRoute
+from .routers.supportTicket import router as support_tickets_router
+from .routers.ratings import router as ratings_router
 from .models import model_loader
 from .dependencies.config import conf
-
 
 app = FastAPI()
 
@@ -20,6 +21,9 @@ app.add_middleware(
 
 model_loader.index()
 indexRoute.load_routes(app)
+
+app.include_router(support_tickets_router, prefix="/api/v1")
+app.include_router(ratings_router, prefix="/api/v1/ratings")
 
 
 if __name__ == "__main__":
